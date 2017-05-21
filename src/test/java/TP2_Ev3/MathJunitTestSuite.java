b96d8c8bad23299ac24676a7de84796e490b087f
alexfdez <alex_95th@hotmail.com>
2017-05-21 19:58:15 +0200

    Add files via upload

---
+++ src/test/java/TP2_Ev3/MathJunitTestSuite.java
@@ -0,0 +1,13 @@
+package TP2_Ev3;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   MathTest.class
+})
+public class MathJunitTestSuite {   
+	
+
+}  	
